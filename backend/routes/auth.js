22b2039cb0c79c533192d7e5606a3956dc478f69
Shivanshgoyal17 <shivanshgoyal2001@gmail.com>
2024-09-06 03:06:50 +0530

    Completed till 46

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,15 +1,38 @@
 const express = require('express')
 const router = express.Router()
 const User = require('../models/User')
+const { body, validationResult } = require('express-validator');
 
 // create a User using : POST "/api/auth". Doesn't require authentication
 
-router.post('/', (req, res)=>{
-    console.log(req.body)
-    // res.send("Hello")
-    const user = User(req.body)
-    user.save()
-    res.send(req.body)
+router.post('/', [
+    body('name', 'Enter a valid name').isLength({min:3}),
+    body('email', 'Enter a valid email').isEmail(),
+    body('password', 'Password must be atleast 5 characters').isLength({min: 5})
+], async (req, res)=>{
+    const errors = validationResult(req)
+    if(!errors.isEmpty()){
+        return res.status(400).json({errors: errors.array()})
+    }
+    try {
+        const user = await User.create({
+          name: req.body.name,
+          email: req.body.email,
+          password: req.body.password
+        });
+        res.json(user);
+      } 
+      catch (error) {
+        if (error.code === 11000) {
+          // Duplicate key error
+          return res.status(400).json({ error: 'Email already exists' });
+        }
+        console.error(error);
+        res.status(500).json({ error: 'Server error' });
+      }
+    
+
+    // res.send(req.body)
 
 })
 
